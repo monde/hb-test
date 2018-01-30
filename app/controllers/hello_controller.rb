5342d8f81b71370f310f6e8be14a3c5d11d58e4d
Mike Mondragon <mikemondragon@gmail.com>
2018-01-30 15:33:26 -0800

    first

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,5 @@
+class HelloController < ApplicationController
+  def index
+    raise 'boom'
+  end
+end
---
+++ config/initializers/honeybadger.rb
@@ -0,0 +1,3 @@
+Honeybadger.config.set(:'connection.host', "localhost")
+Honeybadger.config.set(:'connection.port', "3000")
+Honeybadger.config.set(:'connection.secure', false)
